684bccb970b6f3008f0e950ff5e32ed8388cb945
Mateusz Nowak <mtsznowak9@gmail.com>
2017-01-24 12:46:28 +0100

    add ploter and ResultList view implementation

--- src/main/java/pl/edu/agh/ki/frazeusz/model/ploter/Ploter.java
+++ src/main/java/pl/edu/agh/ki/frazeusz/model/ploter/Ploter.java
@@ -5,7 +5,43 @@ import javax.swing.*;
 /**
  * Created by matwoosh on 14/01/2017.
  */
-public class Ploter extends JFrame {
+public class Ploter {
+
+	private Chart chart;
+	private ResultList resultList;
+	private JPanel chartPanel;
+	private JPanel tablePanel;
+	
+	
+	public Ploter() {
+		this.chartPanel = new JPanel();
+		this.tablePanel = new JPanel();
+		
+		this.resultList = new ResultList();
+		this.chart = new Chart(new String[]{"Fraza 1", "Fraza 2", "Fraza 3"});
+	
+		initializePanels();
+	}
+	
+	private void initializePanels() {
+		tablePanel.add(resultList.createResultTable());
+		
+	}
+	
+	public void addResult(Result result) {
+        resultList.addRow(result);
+        chartPanel.removeAll();
+        chartPanel.add(chart.refresh(result));
+        chartPanel.revalidate();
+    }
+	
+	public JPanel getChartView() {
+		return this.chartPanel;
+	}
+
+	public JPanel getListView() {
+		return this.tablePanel;
+	}
 
 
 }
