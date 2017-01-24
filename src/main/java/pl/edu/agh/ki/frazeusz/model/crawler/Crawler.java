b9a67b77050b2951510ca4183250a0d8dd93a5e6
Mateusz Kopeć <ma7woosh@gmail.com>
2017-01-24 14:57:29 +0100

    Crawler template finished

--- src/main/java/pl/edu/agh/ki/frazeusz/model/crawler/Crawler.java
+++ src/main/java/pl/edu/agh/ki/frazeusz/model/crawler/Crawler.java
@@ -1,9 +1,28 @@
 package pl.edu.agh.ki.frazeusz.model.crawler;
 
+import pl.edu.agh.ki.frazeusz.gui.crawler.CrawlerConfiguration;
+import pl.edu.agh.ki.frazeusz.model.monitor.CrawlerStatus;
+import pl.edu.agh.ki.frazeusz.model.parser.IParser;
+
 /**
  * Created by matwoosh on 14/01/2017.
  */
 public class Crawler {
 
+    private IParser parser;
+    private CrawlerStatus monitor;
+
+    public Crawler(IParser parser, CrawlerStatus monitor) {
+        this.parser=parser;
+        this.monitor = monitor;
+    }
+
+    public void start(CrawlerConfiguration crawlerConfiguration){
+        //TODO implement
+    }
+
+    public void stop(){
+        //TODO implement
+    }
 
 }
