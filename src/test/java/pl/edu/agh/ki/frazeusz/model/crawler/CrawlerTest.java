ace5bd292e0ddd6cadfbb8edc6e87204bb7a81b7
Mateusz Kopeć <ma7woosh@gmail.com>
2017-01-26 01:22:54 +0100

    Test class template added

---
+++ src/test/java/pl/edu/agh/ki/frazeusz/model/crawler/CrawlerTest.java
@@ -0,0 +1,22 @@
+package pl.edu.agh.ki.frazeusz.model.crawler;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by matwoosh on 26/01/2017.
+ */
+public class CrawlerTest {
+
+    @Test
+    public void testCrawler() {
+        // given
+
+        // when
+
+        // then
+
+    }
+
+}
